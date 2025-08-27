3c33aea680b3b13c28938c6f93f7d6d1dfe067f8
Madin Phagami <madinphagami@hotmail.com>
2025-08-27 13:15:36 +1200

    completed

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log()
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+
+let totalHomeScore = 0;
+let totalGuestScore = 0;
+
+function addHomeOne() {
+    totalHomeScore += 1;
+    homeScore.textContent = totalHomeScore;
+}
+function addHomeTwo() {
+    totalHomeScore += 2;
+    homeScore.textContent = totalHomeScore;
+}
+function addHomeThree() {
+    totalHomeScore += 3;
+    homeScore.textContent = totalHomeScore;
+}
+
+function addGuestOne(){
+    totalGuestScore += 1;
+    guestScore.textContent = totalGuestScore;
+}
+function addGuestTwo(){
+    totalGuestScore += 2;
+    guestScore.textContent = totalGuestScore;
+}
+function addGuestThree(){
+    totalGuestScore += 3;
+    guestScore.textContent = totalGuestScore;
+}
